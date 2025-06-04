1c6dd7983531cc2da633466c2df8559c71cc33be
LVas3 <93926031+LVas3@users.noreply.github.com>
2025-06-04 08:57:23 +0300

    Update Чётность числа.py

--- Чётность числа.py
+++ Чётность числа.py
@@ -1,12 +1,1 @@
-try:
-    num_input = input("Введите число: ")
-    num = int(num_input)
-    if num >= 0:
-        if num % 2 == 0:
-            print(f"Число {num} является четным")
-        else:
-            print(f"Число {num} не является четным")
-    else:
-        print("Ошибка: число должно быть неотрицательным")
-except ValueError:
-    print("Ошибка: введено не число")
+print ('Четное' if int(input())%2==0 else 'Нечетное')
